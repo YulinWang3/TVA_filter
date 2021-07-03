cded81f6ab550f25ced6b94a814ebb0a4ad72d49
Yulin Wang <1169717559@qq.com>
2021-07-03 08:52:55 -0400

    Modified filter.py with GUI

--- code/filter.py
+++ code/filter.py
@@ -1,78 +1,84 @@
+import PySimpleGUI as sg
 import pandas as pd
-from prompt_toolkit import prompt
 from xlsxwriter import Workbook
 
-#prompt user to enter source file and target file path
-source_file = prompt('Please enter source file path:\n')
-print('')
-target_file = prompt('Please enter target file path:\n')
+#GUI window design
+sg.theme('Light Blue 2')
+layout = [[sg.Text('Browse your source and target files')],
+          [sg.Text('Source File', size=(8, 1)), sg.Input(key='source'), sg.FileBrowse()],
+          [sg.Text('Target File', size=(8, 1)), sg.Input(key='target'), sg.FileBrowse()],
+          [sg.Text('Results', size=(8, 1)), sg.Input(key='results'), sg.FolderBrowse()],
+          [sg.Submit('Filter Them'), sg.Cancel('Close Program')]]
+window = sg.Window('Filter', layout)
 
-#import target build and retrieve source
-source_table = pd.read_excel (source_file)
-target_table = pd.read_excel (target_file)
+#GUI window
+while True:
+    event, values = window.read()
+    if event in (sg.WINDOW_CLOSED, "Close Program"):break
+    elif event == 'Filter Them':
+        file_1, file_2, results = values['source'], values['target'], values['results']
+        if bool(file_1) == False or bool(file_2) == False:break
+       
+        #import target and source files and input DataFrames
+        source_table = pd.read_excel (file_1)
+        target_table = pd.read_excel (file_2)
+        source_data = pd.DataFrame(source_table)
+        target_data = pd.DataFrame(target_table)
+        i = x = 0
 
-#input DateFrames from target and source excel files
-source_data = pd.DataFrame(source_table)
-target_data = pd.DataFrame(target_table)
-i = x = 0
+        #Create output dataframe tables
+        col_correct = ['Index','Updated TVAs']
+        col_incorrect = ['Index','Not updated TVAs']
+        result_df_correct = pd.DataFrame(index=None, columns=col_correct)
+        result_df_incorrect = pd.DataFrame(index=None, columns=col_incorrect)
+        writer = pd.ExcelWriter(results + '/results.xlsx')
 
-#tables title of TVAs who have/haven't updated to the correct version
-col_correct = ['Index','Updated TVAs']
-col_incorrect = ['Index','Not updated TVAs']
+        #iterate each column of target table
+        for columnName, columnValue in target_data.iteritems():
+            print('')
+            print(columnName)
+           
+            #for chiclet column - allows user to check version and PI separately
+            if columnName == 'chiclet':
+                for index, row in source_data.iterrows():
+                    if str(row[columnName]).find(str(columnValue.values[0])) != -1:
+                        result_df_add = pd.DataFrame([[i+1,str(row['TVA'])]], columns=col_correct)
+                        result_df_correct=result_df_correct.append(result_df_add)
+                        i = i+1
+                    elif str(row[columnName]) != '0-0':
+                        result_df_add = pd.DataFrame([[x+1,str(row['TVA'])]], columns=col_incorrect)
+                        result_df_incorrect=result_df_incorrect.append(result_df_add)
+                        x = x+1
+            #for other columns - only version checking needed
+            else:
+                for index, row in source_data.iterrows():
+                    if row[columnName] == columnValue.values[0]:
+                        result_df_add = pd.DataFrame([[i+1,str(row['TVA'])]], columns=col_correct)
+                        result_df_correct=result_df_correct.append(result_df_add)
+                        i = i+1
+                    elif row[columnName] != 0:
+                        result_df_add = pd.DataFrame([[x+1,str(row['TVA'])]], columns=col_incorrect)
+                        result_df_incorrect=result_df_incorrect.append(result_df_add)
+                        x = x+1
+            
+            #export tables to a excel
+            if i!=0: result_df_correct.to_excel(writer, sheet_name=columnName, startrow=0, startcol=0, index=False)
+            if x!=0: result_df_incorrect.to_excel(writer, sheet_name=columnName, startrow=0, startcol=3, index=False)
+            
+            #print out relavent infomation in terminal
+            if i != 0:print(result_df_correct)
+            print('')
+            if x != 0:print(result_df_incorrect)
+            print('total tester:', i+x)
+            print('correct = ', i)
+            print('incorrect = ', x)
 
-#output dataframes for correct/incorrlect TVAs
-result_df_correct = pd.DataFrame(index=None, columns=col_correct)
-result_df_incorrect = pd.DataFrame(index=None, columns=col_incorrect)
-writer = pd.ExcelWriter('results.xlsx')
+            #clear dataframes and counters
+            result_df_correct = pd.DataFrame(columns=col_correct)
+            result_df_incorrect = pd.DataFrame(columns=col_incorrect)
+            x = i = 0
+            print('')
+            print('')
 
-#iterate each column of target table
-for columnName, columnValue in target_data.iteritems():
-    print('')
-    print(columnName)
-    #for chiclet column
-    if columnName == 'chiclet':
-        for index, row in source_data.iterrows():
-            if str(row[columnName]).find(str(columnValue.values[0])) != -1:
-                result_df_add = pd.DataFrame([[i+1,str(row['TVA'])]], columns=col_correct)
-                result_df_correct=result_df_correct.append(result_df_add)
-                i = i+1
-            elif str(row[columnName]) != '0-0':
-                result_df_add = pd.DataFrame([[x+1,str(row['TVA'])]], columns=col_incorrect)
-                result_df_incorrect=result_df_incorrect.append(result_df_add)
-                x = x+1
-    #for other columns
-    else:
-        for index, row in source_data.iterrows():
-            if row[columnName] == columnValue.values[0]:
-                result_df_add = pd.DataFrame([[i+1,str(row['TVA'])]], columns=col_correct)
-                result_df_correct=result_df_correct.append(result_df_add)
-                i = i+1
-            elif row[columnName] != 0:
-                result_df_add = pd.DataFrame([[x+1,str(row['TVA'])]], columns=col_incorrect)
-                result_df_incorrect=result_df_incorrect.append(result_df_add)
-                x = x+1
-    
-    if i!=0: result_df_correct.to_excel(writer, sheet_name=columnName, startrow=0, startcol=0, index=False)
-    if x!=0: result_df_incorrect.to_excel(writer, sheet_name=columnName, startrow=0, startcol=3, index=False)
-    
-
-
-    #ignore empty tables and print not empty tables
-    if i != 0:print(result_df_correct)
-    print('')
-    if x != 0:print(result_df_incorrect)
-    
-    #print out relavent values
-    print('total tester:', i+x)
-    print('correct = ', i)
-    print('incorrect = ', x)
-
-    #clear dataframes and counters
-    result_df_correct = pd.DataFrame(columns=col_correct)
-    result_df_incorrect = pd.DataFrame(columns=col_incorrect)
-    x = i = 0
-    print('')
-    print('')
-
-
-writer.close()
+        writer.close()#close excel writer
+window.close()#close GUI window
